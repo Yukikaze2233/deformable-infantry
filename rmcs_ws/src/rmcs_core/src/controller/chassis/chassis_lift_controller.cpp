6ee14c2c840ac893afcec90ae3bc983fe332946e
eye-on <haoyeei34@gmail.com>
2025-11-20 21:39:51 +0800

    fixed_lift_

--- rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
@@ -24,16 +24,12 @@ public:
             {5.0, 0.1, 0.05},  
             {5.0, 0.1, 0.05}   
             }
-        {
-        register_input("/remote/joystick/right", joystick_right_);
-        register_input("/remote/switch/right", switch_right_);
-        register_input("/remote/switch/left", switch_left_);
-        
-        register_input("/chassis/lift/left_front_wheel/angle", left_front_wheel_angle_);
-        register_input("/chassis/lift/left_front_wheel/encoder_angle", left_front_wheel_encoder_angle_);
-        register_input("/chassis/lift/left_back_wheel/angle", left_back_wheel_angle_);
-        register_input("/chassis/lift/right_front_wheel/angle", right_front_wheel_angle_);
-        register_input("/chassis/lift/right_back_wheel/angle", right_back_wheel_angle_);
+        {        
+        register_input("/chassis/lift/target_angle", target_angle_);
+        register_input("/chassis/lift/left_front_wheel/encoder_angle", left_front_wheel_angle_);
+        register_input("/chassis/lift/left_back_wheel/encoder_angle", left_back_wheel_angle_);
+        register_input("/chassis/lift/right_front_wheel/encoder_angle", right_front_wheel_angle_);
+        register_input("/chassis/lift/right_back_wheel/encoder_angle", right_back_wheel_angle_);
 
         register_output("/chassis/lift/left_front_wheel/control_torque", left_front_wheel_torque_, nan_);
         register_output("/chassis/lift/left_back_wheel/control_torque", left_back_wheel_torque_, nan_);
@@ -42,53 +38,21 @@ public:
         }
 
     void update() override {
+        double err_lf = *target_angle_ - *left_front_wheel_angle_;
+        double err_lb = *target_angle_ - *left_back_wheel_angle_;
+        double err_rf = *target_angle_ - *right_front_wheel_angle_;
+        double err_rb = *target_angle_ - *right_back_wheel_angle_;
+
+        auto calc_torque = [this](rmcs_core::controller::pid::PidCalculator& pid, double error) {
+            double torque = pid.update(error);
+            return std::clamp(torque, -max_torque_, max_torque_);
+        };
+
+        *left_front_wheel_torque_ = calc_torque(balance_pid[0], err_lf);
+        *left_back_wheel_torque_  = calc_torque(balance_pid[1], err_lb);
+        *right_front_wheel_torque_ = calc_torque(balance_pid[2], err_rf);
+        *right_back_wheel_torque_  = calc_torque(balance_pid[3], err_rb);
         
-        auto switch_right_state = *switch_right_;
-        auto switch_left_state = *switch_left_;
-        
-        auto joystick_right_state = *joystick_right_;
-
-        auto left_front_wheel_angle = *left_front_wheel_angle_;
-        auto left_back_wheel_angle = *left_back_wheel_angle_;
-        auto right_front_wheel_angle = *right_front_wheel_angle_;
-        auto right_back_wheel_angle = *right_back_wheel_angle_;
-
-        
-        // if (switch_right_state == rmcs_msgs::Switch::DOWN && 
-        //     switch_left_state == rmcs_msgs::Switch::DOWN) {
-        //     stop_lift();
-        //     return;
-        // }
-        // else if(switch_right_state == rmcs_msgs::Switch::MIDDLE && 
-        //         switch_left_state == rmcs_msgs::Switch::MIDDLE){
-        //     double target_torque = 2.5/*joystick_right_state.x() * max_torque_*/;
-
-        //     if(std::abs(target_torque) < 1.2){
-        //         target_torque = 0.0;
-        //     }
-
-        //     auto [left_front_compensation, left_back_compensation, right_front_compensation, right_back_compensation] = torque_calculator(left_front_wheel_angle, left_back_wheel_angle, right_front_wheel_angle, right_back_wheel_angle);
-            
-        //     double left_front_target_torque = std::clamp(target_torque + left_front_compensation, -max_torque_, max_torque_);
-        //     double left_back_target_torque = std::clamp(target_torque + left_back_compensation, -max_torque_, max_torque_);
-        //     double right_front_target_torque = std::clamp(target_torque + right_front_compensation, -max_torque_, max_torque_);
-        //     double right_back_target_torque = std::clamp(target_torque + right_back_compensation, -max_torque_, max_torque_);
-            
-        //     *left_front_wheel_torque_ = target_torque/*left_front_target_torque*/;
-        //     *left_back_wheel_torque_ = left_back_target_torque;
-        //     *right_front_wheel_torque_ = right_front_target_torque;
-        //     *right_back_wheel_torque_ = right_back_target_torque;
-            
-        // }
-        // else {
-        //     stop_lift();
-        //     return;
-        // }
-         *left_front_wheel_torque_ = -0.7;
-        RCLCPP_INFO(
-            get_logger(), "Max control torque of wheel motor: %f",
-            *left_front_wheel_encoder_angle_);
-         
     }
 private:
 
@@ -100,25 +64,14 @@ private:
         RCLCPP_INFO(get_logger(), "Stopping lift");
     }
 
-    std::tuple<double, double,double,double> torque_calculator(double angle1,double angle2,double angle3,double angle4){
-        double avg_angle = ( angle1 + angle2 + angle3 + angle4 ) / 4.0;
-        double angle_diff1 = angle1 - avg_angle ;
-        double angle_diff2 = angle2 - avg_angle ;
-        double angle_diff3 = angle3 - avg_angle ;
-        double angle_diff4 = angle4 - avg_angle ;
-        double compensation1 = balance_pid[0].update(angle_diff1);
-        double compensation2 = balance_pid[1].update(angle_diff2);
-        double compensation3 = balance_pid[2].update(angle_diff3);
-        double compensation4 = balance_pid[3].update(angle_diff4);
-        return std::make_tuple(compensation1, compensation2, compensation3, compensation4);
-    }
-
     static constexpr double nan_ = std::numeric_limits<double>::quiet_NaN();
 
     InputInterface<Eigen::Vector2d> joystick_right_;
     InputInterface<rmcs_msgs::Switch> switch_right_;
     InputInterface<rmcs_msgs::Switch> switch_left_;
 
+
+    InputInterface<double> target_angle_;
     InputInterface<double> left_front_wheel_angle_;
     InputInterface<double> left_front_wheel_encoder_angle_;
     InputInterface<double> left_back_wheel_angle_;
