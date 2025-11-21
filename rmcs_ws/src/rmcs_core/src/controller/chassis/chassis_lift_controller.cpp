6ef0fef26303226bf1181ee5d07b88a1afc8de49
eye-on <haoyeei34@gmail.com>
2025-11-21 15:33:13 +0800

    fixed_lift_v3

--- rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
@@ -1,10 +1,9 @@
-
 #include <cstdlib>
 #include <rclcpp/node.hpp>
 #include <rmcs_executor/component.hpp>
 #include <rmcs_msgs/switch.hpp>
 #include <eigen3/Eigen/Dense>
-#include <tuple>
+#include <cmath>
 
 #include "controller/pid/pid_calculator.hpp"
 
@@ -18,96 +17,66 @@ public:
         : Node(
               get_component_name(),
               rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true))
-        , balance_pid{  
-            {2.0, 0.1, 0.05},  
-            {2.0, 0.1, 0.05}, 
-            {2.0, 0.1, 0.05},  
-            {2.0, 0.1, 0.05}   
-            }
-        , overall_pid{10.0, 0.5, 2.0}
-        , angle_tolerance_(0.01)
-        {        
+        , wheel_pids{  
+            {14.0, 0.001, 0.05},  
+            {14.0, 0.001, 0.05},  
+            {14.0, 0.001, 0.05},  
+            {14.0, 0.001, 0.05}   
+        }
+    {        
         register_input("/chassis/lift/target_angle", target_angle_);
-        register_input("/chassis/lift/encoder_angle", encoder_angle_);
-        register_input("/chassis/lift/left_front_wheel/angle", left_front_wheel_angle_);
-        register_input("/chassis/lift/left_back_wheel/angle", left_back_wheel_angle_);
-        register_input("/chassis/lift/right_front_wheel/angle", right_front_wheel_angle_);
-        register_input("/chassis/lift/right_back_wheel/angle", right_back_wheel_angle_);
+        register_input("/chassis/lift/left_front_wheel/encoder_angle", left_front_wheel_angle_);
+        register_input("/chassis/lift/left_back_wheel/encoder_angle", left_back_wheel_angle_);
+        register_input("/chassis/lift/right_front_wheel/encoder_angle", right_front_wheel_angle_);
+        register_input("/chassis/lift/right_back_wheel/encoder_angle", right_back_wheel_angle_);
 
         register_output("/chassis/lift/left_front_wheel/control_torque", left_front_wheel_torque_, nan_);
         register_output("/chassis/lift/left_back_wheel/control_torque", left_back_wheel_torque_, nan_);
         register_output("/chassis/lift/right_front_wheel/control_torque", right_front_wheel_torque_, nan_);
         register_output("/chassis/lift/right_back_wheel/control_torque", right_back_wheel_torque_, nan_);
 
-        overall_pid.output_max = max_torque_ * 0.8;  
-        overall_pid.output_min = -max_torque_ * 0.8;
-        }
+        for (auto& pid : wheel_pids) {
+            pid.output_max = max_torque_;
+            pid.output_min = -max_torque_;
+        } 
+    }
 
     void update() override {
 
         double target = *target_angle_;
-        double current_overall = *encoder_angle_;  
         double lf = *left_front_wheel_angle_;
         double lb = *left_back_wheel_angle_;
         double rf = *right_front_wheel_angle_;
         double rb = *right_back_wheel_angle_;
 
-        double overall_err = target - current_overall;
-        overall_err = std::atan2(std::sin(overall_err), std::cos(overall_err));
-
-        double base_torque = overall_pid.update(overall_err);
-
-        auto [comp_lf, comp_lb, comp_rf, comp_rb] = torque_calculator(lf, lb, rf, rb);
-        comp_lf = -comp_lf;
-        comp_lb = -comp_lb;
-        comp_rf = -comp_rf;
-        comp_rb = -comp_rb;
-
-        auto calc_final_torque = [this](double base, double comp) {
-            double total = base + comp;
-            return std::clamp(total, -max_torque_, max_torque_);
+        auto calc_error = [target](double current) {
+            double err = target - current;
+            return std::atan2(std::sin(err), std::cos(err));  
         };
+        double lf_err = calc_error(lf);
+        double lb_err = calc_error(lb);
+        double rf_err = calc_error(rf);
+        double rb_err = calc_error(rb);
 
-        if (std::abs(overall_err) < angle_tolerance_) {
+        *left_front_wheel_torque_ = wheel_pids[0].update(lf_err);
+        *left_back_wheel_torque_  = wheel_pids[1].update(lb_err);
+        *right_front_wheel_torque_ = wheel_pids[2].update(rf_err);
+        *right_back_wheel_torque_  = wheel_pids[3].update(rb_err);
 
-            *left_front_wheel_torque_ = calc_final_torque(base_torque, comp_lf);
-            *left_back_wheel_torque_  = calc_final_torque(base_torque, comp_lb);
-            *right_front_wheel_torque_ = calc_final_torque(base_torque, comp_rf);
-            *right_back_wheel_torque_  = calc_final_torque(base_torque, comp_rb);
-
-        }
     }
+
 private:
-
     void stop_lift(){
-         *left_front_wheel_torque_ = 0.0;
-          *left_back_wheel_torque_ = 0.0;
+        *left_front_wheel_torque_ = 0.0;
+        *left_back_wheel_torque_ = 0.0;
         *right_front_wheel_torque_ = 0.0;
-         *right_back_wheel_torque_ = 0.0;
+        *right_back_wheel_torque_ = 0.0;
         RCLCPP_INFO(get_logger(), "Stopping lift");
-    }
-    std::tuple<double, double,double,double> torque_calculator(double angle1,double angle2,double angle3,double angle4){
-        double avg_angle = ( angle1 + angle2 + angle3 + angle4 ) / 4.0;
-        double angle_diff1 = angle1 - avg_angle ;
-        double angle_diff2 = angle2 - avg_angle ;
-        double angle_diff3 = angle3 - avg_angle ;
-        double angle_diff4 = angle4 - avg_angle ;
-        double compensation1 = balance_pid[0].update(angle_diff1);
-        double compensation2 = balance_pid[1].update(angle_diff2);
-        double compensation3 = balance_pid[2].update(angle_diff3);
-        double compensation4 = balance_pid[3].update(angle_diff4);
-        return std::make_tuple(compensation1, compensation2, compensation3, compensation4);
     }
 
     static constexpr double nan_ = std::numeric_limits<double>::quiet_NaN();
 
-    InputInterface<Eigen::Vector2d> joystick_right_;
-    InputInterface<rmcs_msgs::Switch> switch_right_;
-    InputInterface<rmcs_msgs::Switch> switch_left_;
-
-
     InputInterface<double> target_angle_;
-    InputInterface<double> encoder_angle_;
     InputInterface<double> left_front_wheel_angle_;
     InputInterface<double> left_back_wheel_angle_;
     InputInterface<double> right_front_wheel_angle_;
@@ -118,11 +87,10 @@ private:
     OutputInterface<double> right_front_wheel_torque_;
     OutputInterface<double> right_back_wheel_torque_;
 
-    rmcs_core::controller::pid::PidCalculator balance_pid[4];
-    rmcs_core::controller::pid::PidCalculator overall_pid;
+    rmcs_core::controller::pid::PidCalculator wheel_pids[4];
 
-    const double max_torque_ = 6.0;
-    const double angle_tolerance_;  
+
+    const double max_torque_ = 6.0;                    
 };
 
 } // namespace rmcs_core::controller::chassis
