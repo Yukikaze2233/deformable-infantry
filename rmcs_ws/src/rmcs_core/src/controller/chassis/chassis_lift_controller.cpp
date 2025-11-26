459cde777dbc850def0fff98b6a1e7d26004735f
eye-on <haoyeei34@gmail.com>
2025-11-26 17:07:48 +0800

    PID velocity test1

--- rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/chassis/chassis_lift_controller.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 
 #include "controller/pid/pid_calculator.hpp"
+#include "std_msgs/msg/int32.hpp"
 
 namespace rmcs_core::controller::chassis {
 
@@ -33,6 +34,16 @@ public:
         register_input("/chassis/lift/right_front_wheel/encoder_angle", right_front_wheel_angle_);
         register_input("/chassis/lift/right_back_wheel/encoder_angle", right_back_wheel_angle_);
 
+        register_input("/chassis/lift/left_front_wheel/velocity", left_front_wheel_velocity_);
+        register_input("/chassis/lift/left_back_wheel/velocity", left_back_wheel_velocity_);
+        register_input("/chassis/lift/right_front_wheel/velocity", right_front_wheel_velocity_);
+        register_input("/chassis/lift/right_back_wheel/velocity", right_back_wheel_velocity_);
+
+        L0 = get_parameter("Initial_length").as_double();
+        Bx = get_parameter("Rod_relative_horizontal_coordinate").as_double();
+        By = get_parameter("Rod_relative_longitudinal_coordinate").as_double();
+        L = get_parameter("Rod_length").as_double();
+
         register_output("/chassis/lift/left_front_wheel/control_torque", left_front_wheel_torque_, nan_);
         register_output("/chassis/lift/left_back_wheel/control_torque", left_back_wheel_torque_, nan_);
         register_output("/chassis/lift/right_front_wheel/control_torque", right_front_wheel_torque_, nan_);
@@ -46,13 +57,11 @@ public:
 
     void update() override {
 
-        double target =  *target_angle_;
-        double lf = *left_front_wheel_angle_;
-        double lb = *left_back_wheel_angle_;
-        double rf = *right_front_wheel_angle_;
-        double rb = *right_back_wheel_angle_;
-
-        target = 35;
+        double target = trapezoidal_calaulator(*target_angle_);
+        chassis_lift_controller_ = create_subscription<std_msgs::msg::Int32>(
+        "/chassis/lift/target_angle", rclcpp::QoS{0}, [this](std_msgs::msg::Int32::UniquePtr&& msg) {
+            init_calculator(std::move(msg));
+        });
 
 
         target = (target < min_angle_) ? min_angle_ : (target > max_angle_) ? max_angle_ : target;
@@ -61,12 +70,16 @@ public:
             double err = target - current;
             return std::atan2(std::sin(err), std::cos(err));  
         };
-        // double lf_err = calc_error(lf);
 
-        double lf_err = target - lf;
-        double lb_err = calc_error(lb);
-        double rf_err = calc_error(rf);
-        double rb_err = calc_error(rb);
+        s_lf = lf + *left_front_wheel_velocity_/(2 * pi) * dt;
+        s_lb = lb + *left_back_wheel_velocity_/(2 * pi) * dt;
+        s_rf = rf + *right_front_wheel_velocity_/(2 * pi) * dt;
+        s_rb = rb + *right_back_wheel_velocity_/(2 * pi) * dt;
+
+        double lf_err = calc_error(s_lf);
+        double lb_err = calc_error(s_lb);
+        double rf_err = calc_error(s_rf);
+        double rb_err = calc_error(s_rb);
 
         *left_front_wheel_torque_ = std::clamp(wheel_pids[0].update(lf_err),-0.8,0.8);
 
@@ -74,9 +87,7 @@ public:
         *left_back_wheel_torque_  = wheel_pids[1].update(lb_err);
         *right_front_wheel_torque_ = wheel_pids[2].update(rf_err);
         *right_back_wheel_torque_  = wheel_pids[3].update(rb_err);
-
-        RCLCPP_INFO(get_logger(), "encode:%f", lf);
-
+        
     }
 
 private:
@@ -88,13 +99,35 @@ private:
         RCLCPP_INFO(get_logger(), "Stopping lift");
     }
 
+    double trapezoidal_calaulator(double alpha) const{
+        double term = Bx * cos(alpha) + By * sin(alpha);
+        double s = term + sqrt(term * term - Bx * Bx - By * By + L * L) - L0;
+        return s;
+    }
+
+    void init_calculator(std_msgs::msg::Int32::UniquePtr){
+        lf = trapezoidal_calaulator(*left_front_wheel_angle_);
+        lb = trapezoidal_calaulator(*left_back_wheel_angle_);
+        rf = trapezoidal_calaulator(*right_front_wheel_angle_);
+        rb = trapezoidal_calaulator(*right_back_wheel_angle_);
+        s_lf = lf;
+        s_lb = lb;
+        s_rf = rf;
+        s_rb = rb;
+    }
+
     static constexpr double nan_ = std::numeric_limits<double>::quiet_NaN();
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr chassis_lift_controller_;
 
     InputInterface<double> target_angle_;
     InputInterface<double> left_front_wheel_angle_;
     InputInterface<double> left_back_wheel_angle_;
     InputInterface<double> right_front_wheel_angle_;
     InputInterface<double> right_back_wheel_angle_;
+    InputInterface<double> left_front_wheel_velocity_;
+    InputInterface<double> left_back_wheel_velocity_;
+    InputInterface<double> right_front_wheel_velocity_;
+    InputInterface<double> right_back_wheel_velocity_;
 
     OutputInterface<double> left_front_wheel_torque_;
     OutputInterface<double> left_back_wheel_torque_;
@@ -106,7 +139,17 @@ private:
     const double min_angle_ ;
     const double max_angle_ ;
 
-    const double max_torque_ = 6.0;                
+    const double max_torque_ = 6.0;
+
+
+    double lf, lb, rf, rb;
+    double s_lf, s_lb, s_rf, s_rb;
+
+    double L0;
+    double Bx, By;
+    double L;
+    double dt = 0.001;
+    double pi = 3.1415926;
 };
 
 } // namespace rmcs_core::controller::chassis
