8f394fc681cfddcb0d4d8b7a9dd4238fa3998064
eye-on <haoyeei34@gmail.com>
2026-01-13 19:04:50 +0800

    test

--- rmcs_ws/src/rmcs_core/src/controller/gimbal/test_gimbal_controller.cpp
+++ rmcs_ws/src/rmcs_core/src/controller/gimbal/test_gimbal_controller.cpp
@@ -15,37 +15,42 @@ public:
               get_component_name(),
               rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true)) {
 
-        threshold_ = get_parameter("threshold").as_double();
+        // threshold_ = get_parameter("threshold").as_double();
+        register_input("/remote/switch/right", switch_right_);
         register_input("/gimbal/yaw/velocity", yaw_velocity_);
         register_input("/gimbal/yaw/target_velocity", yaw_target_velocity_);
 
         register_output("/gimbal/yaw/velocity_error", yaw_velocity_error, 0.0);
         register_output("/gimbal/yaw/controller_torque", yaw_control_torque_, 0.0);
-        csv_file_.init_csv_recorder(csv_name1, csv_name2, csv_name3);
+        // csv_file_.init_csv_recorder(csv_name1, csv_name2, csv_name3);
             
     }
 
     void update() override {
         // *yaw_velocity_error = *yaw_target_velocity_ - *yaw_velocity_;
         T += 0.001;
-        if(std::abs(*yaw_velocity_) > threshold_) {
-            *yaw_control_torque_ += 0.0;
-        }else{
-            *yaw_control_torque_ = T * 10;
-        } 
-        csv_file_.record_data(*yaw_velocity_, *yaw_target_velocity_, *yaw_control_torque_);
+        auto switch_right = *switch_right_;
+        if(switch_right == rmcs_msgs::Switch::UP){
+            if(std::abs(*yaw_velocity_) > threshold_) {
+                *yaw_control_torque_ += 0.0;
+            }else{
+                *yaw_control_torque_ = T * 0.1;
+            } 
+        }
+        // csv_file_.record_data(*yaw_velocity_, *yaw_target_velocity_, *yaw_control_torque_);
     }
 
 private:
     static constexpr double nan_ = std::numeric_limits<double>::quiet_NaN();
 
-    double threshold_ = 0.0;
+    InputInterface<rmcs_msgs::Switch> switch_right_;
+    double threshold_ = 1.0;
     double T =0.0;
     std::string csv_name1 = "yaw_velocity";
     std::string csv_name2 = "yaw_target_velocity";
     std::string csv_name3 = "yaw_control_torque";
 
-    debug::CSVfilesave csv_file_;
+    // debug::CSVfilesave csv_file_;
     InputInterface<double> yaw_velocity_;
     InputInterface<double> yaw_target_velocity_;
 
